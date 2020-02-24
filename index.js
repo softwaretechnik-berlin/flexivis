c590c5634d7f8aa57a466780bd8b0715d15d5322
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2020-02-24 21:00:26 +0100

    Forward to flexivis website

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+if (location.host === "geojson.infrastruktur.link") {
+  const searchParams = new URLSearchParams(location.search);
+  location.replace(
+    `https://flexivis.infrastruktur.link/?url=map:${searchParams.get("url")}`
+  );
+}
+
 import * as riot from "riot";
 import AppComponent from "./app.riot";
 import "./index.css";
