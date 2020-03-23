79254172da84b8a7f1216c2034aeca28ac0492c7
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2020-03-23 22:16:41 +0100

    Remove geojson subdomain redirection

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-if (location.host === "geojson.infrastruktur.link") {
-  const searchParams = new URLSearchParams(location.search);
-  location.replace(
-    `https://flexivis.infrastruktur.link/?url=map:${searchParams.get("url")}`
-  );
-}
-
 import * as riot from "riot";
 
 import Tree from "./tree.riot";
