60284cc70aed6f2fbfddac82db3df6c7107d8d11
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2020-03-23 21:13:31 +0100

    Centralize imports

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ if (location.host === "geojson.infrastruktur.link") {
 }
 
 import * as riot from "riot";
+
+import Tree from "./tree.riot";
+riot.register("tree", Tree);
+import TreeSearch from "./tree-search.riot";
+riot.register("tree-search", TreeSearch);
+import SplitView from "./split-view.riot";
+riot.register("split-view", SplitView);
+import Raw from "./raw.riot";
+riot.register("raw", Raw);
+import LayerMap from "./layer-map.riot";
+riot.register("layer-map", LayerMap);
+import ContentFrame from "./content-frame.riot";
+riot.register("content-frame", ContentFrame);
+
 import AppComponent from "./app.riot";
 import "./index.css";
 
