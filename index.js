469ef297129327c6f3f3def5b5f0540abdef0232
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2020-04-02 00:34:10 +0200

    Move tags to the src directory

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ import "regenerator-runtime/runtime";
 
 import * as riot from "riot";
 
-import Tree from "./tree.riot";
+import Tree from "./src/tags/tree.riot";
 riot.register("tree", Tree);
-import TreeSearch from "./tree-search.riot";
+import TreeSearch from "./src/tags/tree-search.riot";
 riot.register("tree-search", TreeSearch);
-import SplitView from "./split-view.riot";
+import SplitView from "./src/tags/split-view.riot";
 riot.register("split-view", SplitView);
-import Raw from "./raw.riot";
+import Raw from "./src/tags/raw.riot";
 riot.register("raw", Raw);
-import LayerMap from "./layer-map.riot";
+import LayerMap from "./src/tags/layer-map.riot";
 riot.register("layer-map", LayerMap);
-import ContentFrame from "./content-frame.riot";
+import ContentFrame from "./src/tags/content-frame.riot";
 riot.register("content-frame", ContentFrame);
 
-import AppComponent from "./app.riot";
+import AppComponent from "./src/tags/app.riot";
 import "./index.css";
 
 riot.component(AppComponent)(document.getElementById("app"));
