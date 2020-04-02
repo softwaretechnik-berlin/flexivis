019cba3dedaa0bbd91635ce2d837be5b2aa51279
Ben Barnard <barnardb@gmail.com>
2020-04-02 23:13:44 +0200

    Fix relative URLs in vega renderer

--- src/views/vega.js
+++ src/views/vega.js
@@ -7,7 +7,11 @@ export default class VegaHandler extends SourceHandler {
     const div = document.createElement("div");
     div.style.width = "100%";
     div.style.height = "100%";
-    vegaEmbed(div, JSON.parse(source));
+    vegaEmbed(div, JSON.parse(source), {
+      loader: {
+        baseURL: ctx.name ? ctx.description.replace(/(.*)\/.*/, "$1") : undefined
+      }
+    });
     ctx.element.appendChild(div);
   }
 }
