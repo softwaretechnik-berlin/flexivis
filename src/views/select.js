b93d4a8e1ad6c38ce37ba481d0bb909327f295b6
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2020-04-08 20:20:40 +0200

    Rename select handler

--- src/views/select.js
+++ src/views/select.js
@@ -1,6 +1,6 @@
 import { SourceHandler } from "./common";
 
-export default class TextHandler extends SourceHandler {
+export default class SelectHandler extends SourceHandler {
 	handleWithSource(source, ctx) {
 		const config = JSON.parse(source);
 		ctx.riot.mount(ctx.element, config, "item-select");
