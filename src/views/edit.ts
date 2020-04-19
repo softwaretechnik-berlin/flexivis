f67e871b3071b048c56638737f1167e1c881520a
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2020-04-19 16:54:08 +0200

    Use Codejar on the edit view

--- src/views/edit.ts
+++ src/views/edit.ts
@@ -1,24 +1,46 @@
 import { Handler, Context } from "./common";
+import { CodeJar } from "@medv/codejar";
+import { withLineNumbers } from "@medv/codejar/linenumbers";
+import hljs from "highlight.js";
 
 export default class EditHandler implements Handler {
-	async handle(ctx: Context): Promise<void> {
-		const dataSource = ctx.view.resources[0].value;
-		const content = await dataSource.latest;
+  async handle(ctx: Context): Promise<void> {
+    const dataSource = ctx.view.resources[0].value;
+		const initialContent = await dataSource.latest;
 
-		const textarea = document.createElement("textarea");
-		textarea.textContent = content;
-		textarea.style.width = "100%";
-		textarea.style.height = "100%";
+		const wrapper = document.createElement("div");
+		wrapper.style.position = "relative";
 
-		const button = document.createElement("button");
-		button.textContent = "Update";
-		button.style.right = "0";
-		button.style.position = "absolute";
-		button.addEventListener("click", () => {
-			dataSource.latest = textarea.value;
-		});
+    const textarea = document.createElement("div");
+    textarea.classList.add("editor");
+    textarea.style.width = "100%";
+    textarea.style.height = "100%";
 
-		ctx.element.append(textarea);
-		ctx.element.append(button);
-	}
+    const button = document.createElement("button");
+    button.textContent = "Update";
+    button.style.top = "0";
+    button.style.right = "0";
+    button.style.position = "absolute";
+
+		wrapper.append(textarea);
+		wrapper.append(button);
+    ctx.element.append(wrapper);
+
+    const highlight = (editor: HTMLElement): void => {
+      // highlight.js does not trims old tags,
+      // let's do it by this hack.
+      editor.textContent = editor.textContent;
+      try {
+        hljs.highlightBlock(editor);
+      } catch (_) {}
+    };
+    const jar = CodeJar(textarea, withLineNumbers(highlight), {
+      tab: "  ",
+    });
+    jar.updateCode(initialContent);
+
+    button.addEventListener("click", () => {
+      dataSource.latest = jar.toString();
+    });
+  }
 }
