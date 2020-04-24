447c1216faf5aaa869ef73d9b493036b228e0467
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2020-04-24 11:53:38 +0200

    Do a basic Validation of tab/select inputs

--- src/views/select.ts
+++ src/views/select.ts
@@ -1,14 +1,40 @@
 import * as riot from "riot";
 import { Context, SourceHandler, inlineExpandedViews } from "./common";
+import { FlexivisError } from "../flexivis";
+
+type Select = {
+	items: Array<{ id: string }>;
+	parameters: { [key: string]: string | Record<string, unknown> };
+	modals?: { [key: string]: "string" };
+};
+
+const isValid = (object: any): object is Select => {
+	const asSelect = object as Select;
+	return Boolean(
+		asSelect.items && asSelect.items.length !== 0 && asSelect.parameters
+	);
+};
 
 export default class SelectHandler extends SourceHandler {
 	async handleWithSource(source: string, ctx: Context): Promise<void> {
 		const definition = JSON.parse(source);
-		inlineExpandedViews(definition.parameters);
-		riot.mount(
-			ctx.element,
-			{ definition, config: ctx.view.config },
-			"item-select"
-		);
+		if (isValid(definition)) {
+			inlineExpandedViews(definition.parameters);
+			if (definition.modals) {
+				inlineExpandedViews(definition.modals);
+			}
+
+			riot.mount(
+				ctx.element,
+				{ definition, config: ctx.view.config },
+				"item-select"
+			);
+		} else {
+			throw new FlexivisError(
+				"InvalidSelectViewDefinition",
+				"Invalid Select View Definition",
+				`The format ’${source}’ is invalid.`
+			);
+		}
 	}
 }
--- src/views/tab.ts
+++ src/views/tab.ts
@@ -1,15 +1,33 @@
 import * as riot from "riot";
 import { Context, SourceHandler, inlineExpandedViews } from "./common";
+import { FlexivisError } from "../flexivis";
+
+type Tabs = {
+	tabs: { [key: string]: string | Record<string, unknown> };
+};
+
+const isValid = (object: any): object is Tabs => {
+	return Boolean((object as Tabs).tabs);
+};
 
 export default class SelectHandler extends SourceHandler {
 	async handleWithSource(source: string, ctx: Context): Promise<void> {
-		riot.mount(
-			ctx.element,
-			{
-				tabs: inlineExpandedViews(JSON.parse(source).tabs),
-				config: ctx.view.config,
-			},
-			"multi-tab"
-		);
+		const definition = JSON.parse(source);
+		if (isValid(definition)) {
+			riot.mount(
+				ctx.element,
+				{
+					tabs: inlineExpandedViews(definition.tabs),
+					config: ctx.view.config,
+				},
+				"multi-tab"
+			);
+		} else {
+			throw new FlexivisError(
+				"InvalidTabViewDefinition",
+				"Invalid Tab View Definition",
+				`The format ’${source}’ is invalid.`
+			);
+		}
 	}
 }
