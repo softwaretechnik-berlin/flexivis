d12fa40a0093a44bbf3136825a0986a37d5401ed
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2022-01-26 11:06:53 +0100

    Reformat snippet

--- tests/visual/backstop_data/engine_scripts/update-editor-content.js
+++ tests/visual/backstop_data/engine_scripts/update-editor-content.js
@@ -1,9 +1,9 @@
 module.exports = async page => {
-  await page.waitForSelector(".editor");
-  await page.$eval(".editor-content", editor => {
-    editor.textContent += " World!";
-  });
+	await page.waitForSelector(".editor");
+	await page.$eval(".editor-content", editor => {
+		editor.textContent += " World!";
+	});
 
-  // Wait for the changes to be propagated
-  await page.waitForTimeout(1000);
+	// Wait for the changes to be propagated
+	await page.waitForTimeout(1000);
 };
